6c3a3a52ab594baa4e66a833c604d13eaa638c6b
Kirill Gavrilkov <kgavrilkov@yandex.ru>
2020-09-08 09:31:49 +0300

    Completed project

---
+++ scripts/index.js
@@ -0,0 +1,36 @@
+let profileName = document.querySelector('.profile__title');
+let profileJob = document.querySelector('.profile__subtitle');
+let popup = document.querySelector('.popup');
+let popupForm = document.querySelector('.popup__form');
+let inputName = document.querySelector('.popup__item_type_name');
+let inputJob = document.querySelector('.popup__item_type_job');
+let popupSaveButton = document.querySelector('.button_type_save');
+let popupOpenButton = document.querySelector('.button_type_edit');
+let popupCloseButton = document.querySelector('.button_type_close');
+function popupToggle() {
+  popup.classList.toggle('popup_opened');
+  if (popup.classList.contains('popup_opened')) {
+    inputName.value = profileName.textContent;
+    inputJob.value = profileJob.textContent;
+  } else {
+    inputName.value = '';
+    inputJob.value = '';
+  }
+}
+popupToggle();
+function popupCloseByClickOnOverlay() {
+  if (event.target !== event.currentTarget) {
+    return
+  }
+  popupToggle();
+}
+function formSubmitHandler (evt) {
+  evt.preventDefault();
+  profileName.textContent = inputName.value;
+  profileJob.textContent = inputJob.value;
+  popupToggle();
+}
+popupOpenButton.addEventListener('click', popupToggle);
+popupCloseButton.addEventListener('click', popupToggle);
+popup.addEventListener('click', popupCloseByClickOnOverlay);
+popupForm.addEventListener('submit', formSubmitHandler);
