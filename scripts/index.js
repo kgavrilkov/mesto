44727685f6dc60c41412d1c2276ce79b41d1bd0c
Kirill Gavrilkov <kgavrilkov@yandex.ru>
2020-09-09 13:15:50 +0300

    Fixed project

--- scripts/index.js
+++ scripts/index.js
@@ -7,6 +7,7 @@ let inputJob = document.querySelector('.popup__item_type_job');
 let popupSaveButton = document.querySelector('.button_type_save');
 let popupOpenButton = document.querySelector('.button_type_edit');
 let popupCloseButton = document.querySelector('.button_type_close');
+
 function popupToggle() {
   popup.classList.toggle('popup_opened');
   if (popup.classList.contains('popup_opened')) {
@@ -17,19 +18,21 @@ function popupToggle() {
     inputJob.value = '';
   }
 }
-popupToggle();
+
 function popupCloseByClickOnOverlay() {
   if (event.target !== event.currentTarget) {
     return
   }
   popupToggle();
 }
+
 function formSubmitHandler (evt) {
   evt.preventDefault();
   profileName.textContent = inputName.value;
   profileJob.textContent = inputJob.value;
   popupToggle();
 }
+
 popupOpenButton.addEventListener('click', popupToggle);
 popupCloseButton.addEventListener('click', popupToggle);
 popup.addEventListener('click', popupCloseByClickOnOverlay);
